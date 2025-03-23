2962736c3adb6801c7aa986340bfed2915df3381
MikeHapkdoIn <mangelveragomez@gmail.com>
2025-03-23 19:25:30 +0100

    ListaUsuariosWIP

---
+++ js/script.js
@@ -0,0 +1,67 @@
+//obtener datos de la API
+// crear funcion para generar edad aleatoria con spread opertator
+//crear lista contenedor de usuarios en html ul con ID = Usuarios
+//agregar imagen a cada usuario
+//agregar lista de elementos con innerHTML
+
+function edadAleatoria () {
+    return Math.floor(Math.random() *(68 - 18 + 1)) + 18;
+} 
+
+
+fetch (`https://jsonplaceholder.typicode.com/users`) 
+    .then (response => {
+        if (!response.ok) {
+            throw new Error (`Error: ${response.status}`);
+        }
+        return response.json()
+    })
+
+    .then ( data => {
+        const edadUsers = data.map (user => {
+            return{
+                ...user,
+                edad: edadAleatoria (),
+                imagen: `../../assets/img/${user.id}.jpeg`
+            };
+        });
+        
+        console.log(edadUsers)
+        
+
+
+
+    })
+    .catch ( error => {
+        console.error ( "Error al obtener la informacion", error);
+    });
+
+
+function usuariosContainer (usuarios) {
+    const usuariosDom = document.getElementById ("usuarios");
+
+    usuarios.forEach (user =>{
+        const liUsers = document.createElement ("li");
+        liUsers.classList.add ("usuario");
+
+        liUsers.innerHTML = `
+        <img src= "${user.imagen}" alt="${user.name}">
+        <h2>${user.name}</h2>
+        <p>NombreUsuario: ${user.username}</p>
+        <p>Edad: ${user.edad}</p>
+        <p>Telefono: ${user.phone}</p>
+        <p>Email: ${user.email}</p>
+        <p>Empresa: ${user.company.name}</p>
+        <p>Direccion: ${user.address.street}, ${user.address.suite}, ${user.address.city}</p>`;
+
+
+    usuariosDom.appendChild (liUsers);
+            
+                
+            
+        
+    });
+}
+
+    
+
