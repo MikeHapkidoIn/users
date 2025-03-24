62efad8d47cd72895234cff345e1db48b9dba5ba
MikeHapkdoIn <mangelveragomez@gmail.com>
2025-03-24 11:40:03 +0100

    EjercicioDeClase

--- js/script.js
+++ js/script.js
@@ -4,9 +4,13 @@
 //agregar imagen a cada usuario
 //agregar lista de elementos con innerHTML
 
-function edadAleatoria () {
-    return Math.floor(Math.random() *(68 - 18 + 1)) + 18;
-} 
+
+const MIN = 18
+const MAX = 65
+
+const edadAleatoria = () =>  Math.floor(Math.random() *(MAX - MIN) + MIN);
+     
+
 
 
 fetch (`https://jsonplaceholder.typicode.com/users`) 
@@ -22,12 +26,12 @@ fetch (`https://jsonplaceholder.typicode.com/users`)
             return{
                 ...user,
                 edad: edadAleatoria (),
-                imagen: `../../assets/img/${user.id}.jpeg`
+                imagen: `../assets/img/${user.id}.jpeg`
             };
         });
         
         console.log(edadUsers);
-        usuariosContainer(edadUsers);   //esto era lo que me faltaba para que saliesen las img 
+        usuariosContainer(edadUsers);   //esto era lo que me faltaba para que saliesen las img( invocar la funcion)
 
 
     })
@@ -44,14 +48,17 @@ function usuariosContainer (usuarios) {
         liUsers.classList.add ("usuario");
 
         liUsers.innerHTML = `
-        <img src= "${user.imagen}" alt="${user.name}">
-        <h2>${user.name}</h2>
-        <p>NombreUsuario: ${user.username}</p>
-        <p>Edad: ${user.edad}</p>
-        <p>Telefono: ${user.phone}</p>
-        <p>Email: ${user.email}</p>
-        <p>Empresa: ${user.company.name}</p>
-        <p>Direccion: ${user.address.street}, ${user.address.suite}, ${user.address.city}</p>`;
+            
+            <img src= "${user.imagen}" alt="${user.name}">
+            <h2>${user.name}</h2>
+            <p>NombreUsuario: ${user.username}</p>
+            <p>Edad: ${user.edad}</p>
+            <p>Telefono: ${user.phone}</p>
+            <p>Email: ${user.email}</p>
+            <p>Empresa: ${user.company.name}</p>
+            <p>Direccion: ${user.address.street}, ${user.address.suite}, ${user.address.city}</p>`;
+       
+
 
 
     usuariosDom.appendChild (liUsers);
